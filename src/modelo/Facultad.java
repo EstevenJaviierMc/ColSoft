7f18394cc8b1f80d38bbb75eda520cb57b18dde8
EstevenjaviierMc <estevenjaviier@gmail.com>
2019-09-03 10:04:58 -0500

    He creado las variables de la clase Facultad.

--- src/modelo/Facultad.java
+++ src/modelo/Facultad.java
@@ -1,6 +1,36 @@
-
 package modelo;
 
 public class Facultad {
-    
+
+    private int idFacultad;
+    private String nombre;
+    private String estado;
+
+    public int getIdFacultad() {
+        return idFacultad;
+    }
+
+    public void setIdFacultad(int idFacultad) {
+        this.idFacultad = idFacultad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String toString() {
+        return "Facultad nº " + idFacultad + "; " + "Nombre: " + nombre + ", Estado: " + estado + ".";
+    }
 }
